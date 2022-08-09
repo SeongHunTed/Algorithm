309eabb6507c3b7b5062b9c6be719b8a5e4a7975
SeongHunTed <4047ksh@naver.com>
2022-08-10 01:52:05 +0900

    Implementation : Prac 4-2 Solution

--- Implementation/#2.py
+++ Implementation/#2.py
@@ -1,28 +1,39 @@
-user_hour = int(input())
+# user_hour = int(input())
+#
+# hour = 0
+# minute = 0
+# second = 0
+# count = 0
+#
+# while True:
+#     if hour == user_hour and minute == 59 and second == 59:
+#         break
+#     if hour % 10 == 3 or minute % 10 == 3 or second % 10 == 3:
+#         count += 1
+#     elif minute // 10 == 3 or second // 10 == 3:
+#         count += 1
+#
+#     second += 1
+#
+#     if second == 60:
+#         minute += 1
+#         second = 0
+#     if minute == 60:
+#         hour += 1
+#         minute = 0
+#
+# print(count)
 
-hour = 0
-minute = 0
-second = 0
+# Solution
+
+h = int(input())
+
 count = 0
 
-while True:
-    if hour == user_hour and minute == 59 and second == 59:
-        break
-    if hour % 10 == 3 or minute % 10 == 3 or second % 10 == 3:
-        count += 1
-    elif minute // 10 == 3 or second // 10 == 3:
-        count += 1
-
-    second += 1
-
-    if second == 60:
-        minute += 1
-        second = 0
-    if minute == 60:
-        hour += 1
-        minute = 0
-
+for i in range(h+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
 
 print(count)
-
-
