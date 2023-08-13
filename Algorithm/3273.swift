dd6f59e2429906b05f4ed9f0026748ebd2d46843
SeongHunTed <4047ksh@naver.com>
2023-08-14 01:58:58 +0900

    3273

---
+++ Algorithm/3273.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by Hoon on 2023/08/14.
+//
+
+import Foundation
+
+let N = readLine()!
+var array = readLine()!.components(separatedBy: " ").map { Int($0)! }.sorted()
+let x = Int(readLine()!)!
+
+var count = 0
+var start = 0
+var last = array.count - 1
+
+while start < last {
+    let target = array[start] + array[last]
+    
+    if target == x {
+        count += 1
+        start += 1
+        last -= 1
+    } else if target > x {
+        last -= 1
+    } else {
+        start += 1
+    }
+}
+
+print(count)
---
+++ Algorithm/maib.swift
@@ -0,0 +1,8 @@
+//
+//  maib.swift
+//  Algorithm
+//
+//  Created by Hoon on 2023/08/14.
+//
+
+import Foundation
