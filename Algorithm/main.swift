9590c9b726eebe449c4dffddb9b519889593b253
SeongHunTed <4047ksh@naver.com>
2023-08-14 11:25:26 +0900

    10807

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+let N = Int(readLine()!)!
+let arr = readLine()!.components(separatedBy: " ").map { Int($0)! }
+let v = Int(readLine()!)!
+
+var dict = [Int: Int]()
+
+arr.forEach { item in
+    dict[item, default: 0] += 1
+}
+
+print(dict[v, default: 0])
