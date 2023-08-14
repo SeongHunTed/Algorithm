a329e997932f49713bd0ec75ab1febffa3ce307a
SeongHunTed <4047ksh@naver.com>
2023-08-14 11:51:16 +0900

    13300

---
+++ Algorithm/10807.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by Hoon on 2023/08/14.
+//
+
+//import Foundation
+//
+//let N = Int(readLine()!)!
+//let arr = readLine()!.components(separatedBy: " ").map { Int($0)! }
+//let v = Int(readLine()!)!
+//
+//var dict = [Int: Int]()
+//
+//arr.forEach { item in
+//    dict[item, default: 0] += 1
+//}
+//
+//print(dict[v, default: 0])
