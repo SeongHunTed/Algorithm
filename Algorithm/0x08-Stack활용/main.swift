8300d7a2d751a6b3fbfbd7215010969da1660e60
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2024-01-09 16:30:56 +0900

    [BOJ] 2504 괄호의 값

--- Algorithm/0x08-Stack활용/main.swift
+++ Algorithm/0x08-Stack활용/main.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+func parenthesisCount(parenthesises: [String]) -> Int {
+	var stack = [String]()
+	var value = 1
+	var result = 0
+	var isValid = true
+	
+	for i in parenthesises.indices {
+		if parenthesises[i] == "(" {
+			value *= 2
+			stack.append(parenthesises[i])
+		} else if parenthesises[i] == "[" {
+			value *= 3
+			stack.append(parenthesises[i])
+		} else if parenthesises[i] == ")" {
+			if stack.isEmpty || stack.last != "(" {
+				isValid.toggle()
+				break
+			}
+			
+			if parenthesises[i-1] == "(" {
+				result += value
+			}
+			stack.removeLast()
+			value /= 2
+		} else if parenthesises[i] == "]" {
+			if stack.isEmpty || stack.last != "[" {
+				isValid.toggle()
+				break
+			}
+			
+			if parenthesises[i-1] == "[" {
+				result += value
+			}
+			stack.removeLast()
+			value /= 3
+		}
+	}
+	
+	if !isValid || !stack.isEmpty {
+		return 0
+	} else {
+		return result
+	}
+}
+
+let parenthesises = readLine()!.map { String($0) }
+print(parenthesisCount(parenthesises: parenthesises))
