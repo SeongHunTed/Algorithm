552327f3aac588ce7635b460508e22f8319cc0a6
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2024-02-14 17:23:21 +0900

    [BOJ] 2193 이친수

---
+++ Algorithm/0x10-DP/2193.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+def pinaryNumber(n):
+    dp = [0] * 91
+    dp[1] = 1
+    dp[2] = 1
+
+    if n > 2:
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+    print(dp[n])
+
+n = int(input())
+pinaryNumber(n)
