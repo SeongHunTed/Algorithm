7da3cfec9d9ddfc11b32f824cbbc2ce2620f2d55
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2024-02-22 17:17:05 +0900

    [PGS] 최솟값 만들기 -  30분

--- Algorithm/Programmers/Level2/12900.py
+++ Algorithm/Programmers/Level2/12900.py
@@ -18,3 +18,25 @@ def solution(n):
     dp[2] = 2
     answer = recursion(n)
     return answer
+
+from itertools import permutations
+
+# 주어진 두 리스트
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+# 결과를 저장할 리스트
+result = []
+
+# list1의 원소를 기준으로 모든 순열을 생성
+print(list(permutations(list2)))
+# for perm in permutations(list2):
+#     case = []
+#     # 각 순열에 대해 list1의 원소와 짝을 지어 새로운 쌍을 생성
+#     for i in range(len(list1)):
+#         case.append([list1[i], perm[i]])
+#     result.append(case)
+
+# # 생성된 모든 경우의 수 출력
+# for r in result:
+#     print(r)
---
+++ Algorithm/Programmers/Level2/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+for perm in permutations(list2):
+#     case = []
+#     # 각 순열에 대해 list1의 원소와 짝을 지어 새로운 쌍을 생성
+#     for i in range(len(list1)):
+#         case.append([list1[i], perm[i]])
+#     result.append(case)
+
+# # 생성된 모든 경우의 수 출력
+# for r in result:
+#     print(r)
