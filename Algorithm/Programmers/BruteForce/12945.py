dc60b1ac51431bfa5d202f461feefbd41fa250de
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2024-02-22 16:29:59 +0900

    [PGS] 피보나치 수 -  10분

---
+++ Algorithm/Programmers/BruteForce/12945.py
@@ -0,0 +1,19 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+global dp
+dp = [-1] * 100001
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if dp[n] != -1:
+        return dp[n]
+    dp[n] = (fibonacci(n-2) + fibonacci(n-1)) % 1234567
+    return dp[n] % 1234567
+
+def solution(n):
+    answer = fibonacci(n)
+    return answer
