a0816da76daf3fc1ab5704a23f216c2b6485ee1d
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2024-02-26 23:50:26 +0900

    [PGS] 다음 큰 숫자 -  10분

---
+++ Algorithm/Programmers/Level2/12911.py
@@ -0,0 +1,11 @@
+def solution(n):
+    
+    nextNumber = n
+    
+    while True:
+        nextNumber += 1
+        countOne = bin(n)[2:].count('1')
+        
+        if countOne == bin(nextNumber)[2:].count('1'):
+            return nextNumber
+        
