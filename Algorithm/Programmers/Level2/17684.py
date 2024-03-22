62cf1e73905f013c3e482ba19e8c3bd57349aee2
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2024-03-22 17:33:54 +0900

    [PGS] [3차] 압축 - 60분

--- Algorithm/Programmers/Level2/17684.py
+++ Algorithm/Programmers/Level2/17684.py
@@ -1,29 +1,30 @@
 def solution(msg):
-    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 's', 'y', 'z']
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     
     myDict = dict()
     for i in range(1, len(alphabet) + 1):
         myDict[alphabet[i-1].upper()] = i
     
-    index = 0
-    nextNumber = 27
+    l = 0
+    r = 1
+    value = 27
     answer = []
-    i = 1
     
     while True:
-        while msg[index:index+i] in myDict:
-            i += 1
-            if index + i - 1 > len(msg):
-                break
-        answer.append(myDict[msg[index:index+i-1]])
-        
-        if index + i - 1 > len(msg):
+        if r > len(msg):
+            if msg[l:] in myDict:
+                answer.append(myDict[msg[l:]])
             break
+        key = msg[l:r]
         
-        if msg[index:index+i] not in myDict:
-            myDict[msg[index:index+i]] = nextNumber
-            nextNumber += 1
-            index += i - 1
-            i = 1
-            
+        if key in myDict:
+            r += 1
+            continue
+        else:
+            answer.append(myDict[msg[l:r-1]])
+            myDict[key] = value
+            value += 1
+            l = r - 1
+            r = l + 1
+
     return answer
