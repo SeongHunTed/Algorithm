ea18bfcdf4c37a9ac03f7713c2b906fcae083292
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2024-03-27 02:04:42 +0900

    [PGS] 땅따먹기 - 60분

--- Algorithm/Programmers/Level2/12913.py
+++ Algorithm/Programmers/Level2/12913.py
@@ -1,7 +1,13 @@
+# bottom-up 방식의 DP?
 def solution(land):
-    answer = 0
 
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
-    return answer
+    dp = [[-1] * 4 for _ in range(len(land))]
+    dp[-1] = land[-1]
+    
+    for i in range(len(land)-2, -1, -1):
+        for j in range(4):
+            before = dp[i+1].copy()
+            before[j] = -1
+            dp[i][j] = max(dp[i][j], max(before) + land[i][j])
+    
+    return max(dp[0])
