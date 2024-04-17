f0c3bff39ca5b9181b3aab5db207ff8bbf05ebc3
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2024-04-17 20:36:00 +0900

    [BOJ] 24479 알고리즘 수업 - 깊이 우선 탐색 1 - 15분

---
+++ Algorithm/DFS/24479.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+n, m, r = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+count = 1
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for item in graph:
+    item.sort()
+
+print(graph)
+
+def dfs(start):
+    global count
+    visited[start] = count
+
+    for node in graph[start]:
+        if not visited[node]:
+            count += 1
+            dfs(node)
+
+dfs(r)
+
+for i in range(1, n+1):
+    print(visited[i])
