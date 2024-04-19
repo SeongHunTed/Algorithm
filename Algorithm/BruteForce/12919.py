a39829d8381456da58325e58310504bd03049ecb
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2024-04-19 17:27:19 +0900

    [BOJ] 12919 A와 B - 30분

---
+++ Algorithm/BruteForce/12919.py
@@ -0,0 +1,20 @@
+import sys
+
+s = list(input())
+t = list(input())
+
+def process(t):
+    if t == s:
+        print(1)
+        exit()
+    if len(t) == 0:
+        return 0
+    
+    if t[-1] == 'A':
+        process(t[:-1])
+
+    if t[0] == 'B':
+        process(t[1:][::-1])
+    
+process(t)
+print(0)
